dc67433bef22f252d88cab9aeee7562ba1a3a8a9
unknown <lucas.g.apostolo@gmail.com>
2023-07-15 21:34:00 -0300

    ignite Feed

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,29 +1,54 @@
-import styles from './Post.module.css';
+import styles from "./Post.module.css";
 
 const Post = () => {
-    return ( 
-        <article className={styles.post}>
-            <header>
-                <div className={styles.author}>
-                    <img className={styles.avatar} src="https://github.com./MikaelBraghini.png" />
-                    <div className={styles.authorInfo}>
-                        <strong>Mikael Braghni</strong>
-                        <span>Viadinho pao com ovo</span>
-                    </div>
-                </div>
-                <time title='11 de Maio às 21:47' dateTime="2022-05-11 08:13:30">Publicado há 1h</time>
-            </header>
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img
+            className={styles.avatar}
+            src="https://github.com./MikaelBraghini.png"
+          />
+          <div className={styles.authorInfo}>
+            <strong>Mikael Braghni</strong>
+            <span>Viadinho pao com ovo</span>
+          </div>
+        </div>
+        <time title="11 de Maio às 21:47" dateTime="2022-05-11 08:13:30">
+          Publicado há 1h
+        </time>
+      </header>
 
-            <div className={styles.content}>
-                <p>👉Fala galeraa 👋
-                    <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-                    <p>👉 <a>jane.design/doctorcare</a></p>
-                    <p><a>#novoprojeto #nlw #rocketseat</a></p>
-                </p>
-            </div>
-        </article>
+      <div className={styles.content}>
+        <p>
+          👉Fala galeraa 👋
+          <p>
+            Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
+            no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare
+            🚀
+          </p>
+          <p>
+            👉 {' '}<a>jane.design/doctorcare</a>
+          </p>
+          <p>
+            <a>#novoprojeto</a>{' '}
+            <a>#nlw</a> {' '}
+            <a>#rocketseat</a>
+          </p>
+        </p>
+      </div>
 
-     );
-}
- 
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+        
+        <textarea placeholder="Deixe um comentario"/>
+        
+        <footer>
+            <button type="submit">Publicar</button>
+        </footer>
+      </form>
+    </article>
+  );
+};
+
 export default Post;
